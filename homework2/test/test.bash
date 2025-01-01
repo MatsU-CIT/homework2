a43de0222d7cc6dcc625ff6add242ac1c892f94b
Yusuke Matsumoto <yusuke20050315@gmail.com>
2025-01-01 19:46:39 +0900

    ROS2test

--- homework2/test/test.bash
+++ homework2/test/test.bash
@@ -3,28 +3,28 @@ set -e
 
 # ROS 2 ワークスペースのセットアップ
 source /opt/ros/humble/setup.bash
-source $1/ros2_ws/install/setup.bash
+source ~/ros2_ws/install/setup.bash
 
-# 必要なディレクトリへ移動する
-cd $1/ros2_ws
+# 必要なディレクトリへ移動
+cd ~/ros2_ws
 
-# ビルド
+# ビルド確認
 colcon build --packages-select homework2
 
-# ビルド結果をソース
+# ビルド後のワークスペースをソース
 source install/setup.bash
 
-# テスト用ROS 2ノードをバックグラウンドで起動
+# weather_publisherノードをバックグラウンドで起動
 ros2 run homework2 weather_publisher &
 NODE_PID=$!
 
-# ノード起動まで待機
+# ノードの起動確認用に少し待機
 sleep 5
 
-# weather_infoトピックが正しく動作しているかの確認
+# weather_infoトピックからデータを受信して確認
 RESULT=$(timeout 10 ros2 topic echo /weather_info -n 1)
 
-# 取得したデータを確認
+# 取得したデータに期待される文字列が含まれているか確認
 if echo "$RESULT" | grep -q "指定地点の現在の天気は:"; then
   echo "Weather node test passed."
   kill $NODE_PID
