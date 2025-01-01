a3246585a1e639ae8876d9a5e04a31ba5d4c3f09
Yusuke Matsumoto <yusuke20050315@gmail.com>
2025-01-01 21:13:05 +0900

    node

--- homework2/homework2/weather.py
+++ homework2/homework2/weather.py
@@ -1,3 +1,5 @@
+# SPDX-FileCopyrightText: 2024 Yusuke Matsumoto s23c1134bg@s.chibakoudai.jp
+# SPDX-License-Identifier: BSD-3-Clause
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
