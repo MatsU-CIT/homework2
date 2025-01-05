820fa67f0b5faa8c06abbf9b38d13b603ddf5fa3
Yusuke Matsumoto <yusuke20050315@gmail.com>
2025-01-05 20:16:35 +0900

    launch

--- launch/talk_listen.launch.py
+++ launch/talk_listen.launch.py
@@ -8,9 +8,9 @@ import launch_ros.actions
 
 def generate_launch_description():
 
-    weather_publisher = launch_ros.actions.Node(
+    weather = launch_ros.actions.Node(
         package='homework2',
-        executable='weather_publisher',
+        executable='weather',
         output='screen',
     )
     listener = launch_ros.actions.Node(
