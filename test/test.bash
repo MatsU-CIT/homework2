6e8f013303d0ecfe28c8a1803e2d4d4a562e11c2
Yusuke Matsumoto <yusuke20050315@gmail.com>
2025-01-05 21:41:35 +0900

    ROS2 test

--- test/test.bash
+++ test/test.bash
@@ -8,7 +8,7 @@ dir=~
 cd $dir/ros2_ws
 colcon build
 source $dir/.bashrc
-timeout 10 ros2 launch homework2 talk_listen.launch.py > /tmp/homework2.log
+timeout 20 ros2 launch homework2 talk_listen.launch.py > /tmp/homework2.log
 
 cat /tmp/homework2.log |
 grep 'yeah'
